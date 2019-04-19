52fbf0d7d59aaa85943a08534ff5458ce438c865
Ekkawut Saengsawasdikul <ekkawut.saengsawasdikul@kingpower.com>
2019-04-19 11:55:36 +0700

    Hello First Library

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="helloworld-library",
+    name="Noon-helloworld-library",
     version='1.0',
     package_dir={'' : 'src'},
     packages=['HelloWorld'],
-    url='https://github.com/up1/demo-helloworld-library',
-    author='Somkiat',
-    author_email='somkiat.p@gmail.com',
+    url='https://github.com/ekkawuts/demo-helloworld-library',
+    author='Ekkawut',
+    author_email='ekkawut.saeng@hotmail.com',
 )
